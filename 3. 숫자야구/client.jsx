27231269a8b30a29c59cf19fc92813ef4e41fc6f
고영일 <ykoh728@gmail.com>
2020-08-07 22:05:07 +0900

    make numberBaseball

---
+++ 3. 숫자야구/client.jsx
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const { hot } = require('react-hot-loader/root');
+import NumberBaseball from './NumberBaseball';
+
+const Hot = hot(NumberBaseball);
+
+ReactDOM.render(<Hot />, document.querySelector('#root'));
